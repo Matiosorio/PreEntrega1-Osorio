2b30800d86251166dfb2e112d73c74ad316ca128
Matias Osorio <matiasosorio@gmail.com>
2023-07-02 21:56:35 -0300

    Quinto commit: Se agregaron productos al asynmock. Se cambiaron las cards

--- src/componentes/Item/Item.jsx
+++ src/componentes/Item/Item.jsx
@@ -1,14 +1,14 @@
 import './Item.css'
 import { Link } from 'react-router-dom';
 
-const Item = ({ id, nombre, precio, img }) => {
+const Item = ({ id, nombre, precio, img, stock }) => {
   return (
     <div className="cardProducto">
       <img src={img} className="card-img" />
       <div className="info">
-        <h3 style={{ fontSize: '17px' }}>Nombre: {nombre}</h3>
+        <h3 style={{ fontSize: '17px' }}> {nombre}</h3>
         <p style={{ fontSize: '17px' }}>Precio: $ {precio}</p>
-        <p style={{ fontSize: '17px' }}>ID: {id}</p>
+        <p style={{ fontSize: '17px' }}>Stock disponible: {stock}</p>
         <Link to={`/item/${id}`}>
           <button className="boton-detalle"> Ver Detalle </button>
         </Link>
