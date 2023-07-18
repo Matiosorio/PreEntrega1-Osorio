96af25885b409ba699384db6d58cbabccdb909da
Matias Osorio <matiasosorio@gmail.com>
2023-07-18 18:52:18 -0300

    Noveno commit: Se corrigio el Navbar

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -7,21 +7,25 @@ import './NavBar.css'
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" variant="light" style={{ borderBottom: '1px solid black' }}>
+    <Navbar bg="light" variant="light" expand="lg" style={{ borderBottom: '1px solid black' }}>
       <Container>
         <Link to="/">
           <Navbar.Brand><img src="../img/logo.png" alt="Logo Distrimax" className="logo-img" /></Navbar.Brand>
         </Link>
+        
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
-        <Nav className="me-auto">
-          <NavLink to="/categoria/1" className="navbar-link"> Limpieza </NavLink>
-          <NavLink to="/categoria/2" className="navbar-link" > Pileta </NavLink>
-          <NavLink to="/categoria/3" className="navbar-link" > Accesorios </NavLink>
-          <NavLink to="/categoria/4" className="navbar-link" > Insecticidas </NavLink>
-          <NavLink to="/categoria/5" className="navbar-link" > Papeles </NavLink>
-          <NavLink to="/categoria/6" className="navbar-link" > Lavado </NavLink>
-        </Nav>
-        <CartWidget />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to="/categoria/1" className="navbar-link"> Limpieza </NavLink>
+            <NavLink to="/categoria/2" className="navbar-link"> Pileta </NavLink>
+            <NavLink to="/categoria/3" className="navbar-link"> Accesorios </NavLink>
+            <NavLink to="/categoria/4" className="navbar-link"> Insecticidas </NavLink>
+            <NavLink to="/categoria/5" className="navbar-link"> Papeles </NavLink>
+            <NavLink to="/categoria/6" className="navbar-link"> Lavado </NavLink>
+          </Nav>
+          <CartWidget />
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   )
