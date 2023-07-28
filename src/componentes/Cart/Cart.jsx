f95422b6a85c18c434f7b27290f97111a90b4464
Matias Osorio <matiasosorio@gmail.com>
2023-07-28 00:26:23 -0300

    Decimo cuarto commit: Se modifico el cartItem y cart. Se opto por un sistema de tablas para mostrar los productos en el carrito

--- src/componentes/Cart/Cart.jsx
+++ src/componentes/Cart/Cart.jsx
@@ -21,15 +21,27 @@ const Cart = () => {
 
     return (
         <div className="carrito-container">
-            {carrito.map((producto) => (
-                <CartItem key={producto.item.id} item={producto.item} cantidad={producto.cantidad} img={producto.item.img} />
-            ))}
-            <h3>Total: $ {totalArreglado} </h3>
-            <h3>Cantidad total: {cantidadTotal} </h3>
-            <button className="boton-cart" onClick={() => vaciarCarrito()}> Vaciar Carrito </button>
-            <Link className='link-finalizar' to="/checkout"> Finalizar Compra</Link>
-        </div>
-    )
+            <table className="cart-table">
+        <thead>
+          <tr>
+            <th>Producto</th>
+            <th>Precio</th>
+            <th>Cantidad</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {carrito.map((producto) => (
+            <CartItem key={producto.item.id} item={producto.item} cantidad={producto.cantidad} img={producto.item.img} />
+          ))}
+        </tbody>
+      </table>
+      <h3>Total: $ {total.toFixed(2)}</h3>
+      <h3>Cantidad total: {cantidadTotal}</h3>
+      <button className="boton-cart" onClick={() => vaciarCarrito()}>Vaciar Carrito</button>
+      <Link className='link-finalizar' to="/checkout">Finalizar Compra</Link>
+    </div>
+  );
 }
 
 export default Cart
--- src/componentes/CartItem/CartItem.jsx
+++ src/componentes/CartItem/CartItem.jsx
@@ -1,24 +1,27 @@
 import { useContext } from "react"
 import { CarritoContext } from "../../context/CarritoContext"
 import './CartItem.css'
+import { Table } from 'react-bootstrap';
 
 
 const CartItem = ({item, cantidad, img}) => {
     const {eliminarProducto} = useContext(CarritoContext);
 
     return (
-      <div className="cart-item-container">
-          <div className="item-details">
-              <img src={img} className="img-carrito" />
-              <div className="item-info">
-                  <h4> {item.nombre} </h4>
-                  <p> Cantidad: {cantidad} </p>
-                  <p> Precio: $ {item.precio} </p>
-              </div>
-          </div>
-          <button className="boton-eliminar" onClick={() => eliminarProducto(item.id)}> Eliminar </button>
-      </div>
-  );
+        <tr>
+          <td>
+            <div className="item-info">
+              <img src={img} className="img-carrito" alt={item.nombre} />
+              {item.nombre}
+            </div>
+          </td>
+          <td>$ {item.precio}</td>
+          <td>{cantidad}</td>
+          <td>
+            <button className="boton-eliminar" onClick={() => eliminarProducto(item.id)}>Eliminar</button>
+          </td>
+        </tr>
+      );
 }
 
 export default CartItem
